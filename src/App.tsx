06d777671198b5ba77c609b8bafb4f428cd7b2fb
Amador Ollarzabal <amadorollarzabal@Amadors-MBP.attlocal.net>
2022-10-24 20:38:12 -0500

    added input field. added css for input field. added todo model and todo states

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 import { InputField } from './components/InputField';
-
+import {Todo} from './components/model';
 
 const App: React.FC = ()=> {
+  const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] =  useState<Todo[]>([]);
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if(todo){
+      setTodos([...todos, {todo: todo,id: Date.now(), isDone: false}])
+      setTodo('')
+    }
+  };
+console.log(todos)
   return (
     <div className="App">
       <span className='heading'>Todo</span>
+      <InputField 
+        todo={todo} 
+        setTodo={setTodo} 
+        handleAdd={handleAdd} 
+      />
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import './style.css'
 
 interface InputFieldProps {
-
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
 }
 
-export const InputField: React.FC<InputFieldProps> = ({}) => {
+export const InputField: React.FC<InputFieldProps> = ({todo, setTodo, handleAdd}) => {
         return (
-            <form className='input'>
-                <input type="input" placeholder='todo task' className='input__box'/>
-                <button className='input__submit' type='submit'>Add</button>
+            <form className='input' onSubmit={handleAdd}>
+                <input 
+                    type="input" 
+                    placeholder='todo task' 
+                    className='input__box'
+                    value={todo}
+                    onChange={(e)=>setTodo(e.target.value)}
+                />
+                <button className='input_submit' type='submit'>Add</button>
             </form>
         );
 }
